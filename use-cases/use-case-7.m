644bdaf2293d84eb18e9739991e75e2e8a262e67
lameeq <108026207+lameeq@users.noreply.github.com>
2023-03-03 14:42:46 +0000

    Create use-case-7.m

---
+++ use-cases/use-case-7.m
@@ -0,0 +1,53 @@
+# USE CASE: 4 Update an employee's details so that employee's details are kept up-to-date
+## CHARACTERISTIC INFORMATION
+
+### Goal in Context
+
+As an HR advisor I want to update an employee's details so that employee's details are kept up-to-date.
+
+### Scope
+
+Company.
+
+### Level
+
+Primary task.
+
+### Preconditions
+
+We know the role.  Database contains current employee salary data.
+
+### Success End Condition
+
+An employee's details data will be up-to-date.
+
+### Failed End Condition
+
+An employee's details data will be outdated.
+
+### Primary Actor
+
+HR Advisor.
+
+### Trigger
+
+A emplyee's provides the company with new details
+
+## MAIN SUCCESS SCENARIO
+
+1. Information are updatet.
+2. HR advisor recives up-to-date information about employee.
+
+
+## EXTENSIONS
+
+3. **Role does not exist**:
+    1. HR advisor informs finance no role exists.
+
+## SUB-VARIATIONS
+
+None.
+
+## SCHEDULE
+
+**DUE DATE**: Release 1.0
