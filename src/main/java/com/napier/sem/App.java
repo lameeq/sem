41ee8ce9fe760de4b3262615eb72d7220a99bfee
40624131 <40624131@live.napier.ac.uk>
2023-03-10 17:09:26 +0000

    Added unit tests

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,9 @@
 package com.napier.sem;
 
+
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class App
 {
@@ -68,6 +71,9 @@ public class App
             }
         }
     }
+
+
+
     public static void main(String[] args)
     {
 
@@ -82,4 +88,34 @@ public class App
 
 
     }
+
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+
+        // Check employees is not null
+        if (employees == null)
+        {
+            System.out.println("No employees");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+
+            if (emp == null)
+                continue;
+
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
+
 }
