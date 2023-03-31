8ae0c22a3a8018fa95556d50263ec4d95a6a4940
neil <40416093@live.napier.ac.uk>
2023-03-31 16:39:20 +0100

    Lab 7 fixes

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -33,9 +33,9 @@ public class App
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(5000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
